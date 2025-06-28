67369a56a7d6bbe97a3bc79a15c96bcc7b4d9dc5
Vitor Esteves <vitordasilvaesteves@gmail.com>
2025-06-28 11:53:40 -0300

    CRUD em memória

--- node/javascript/index.js
+++ node/javascript/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+app.use(express.json());
+
+let users = [
+  {
+    id: 1,
+    name: 'Vitor',
+    email: 'teste1@teste.com',
+    password: 'teste123',
+  },
+  {
+    id: 2,
+    name: 'Joana',
+    email: 'teste2@teste.com',
+    password: 'teste123',
+  },
+];
 
 // RESTFUL API - REST
 // JSON - Linguagem universal para gerenciamento de dados
@@ -21,26 +37,57 @@ const PORT = 8000;
  *
  */
 app.get('/user', (req, res) => {
+  let result = users;
+  if (req.query.name) {
+    result =
+      result.filter((user) => user.name === req.query.name) ||
+      'Pessoa não existe';
+  }
+
   res.json({
-    msg: 'Trazer todos os usuários da minha base',
+    data: result,
+  });
+});
+
+app.get('/user/:id', (req, res) => {
+  const result = users.find((user) => user.id === parseInt(req.params.id, 10));
+
+  res.json({
+    data: result,
   });
 });
 
 app.post('/user', (req, res) => {
+  users.push({
+    id: users.length + 1,
+    ...req.body,
+  });
   res.json({
-    msg: 'Criar um usuário na minha base',
+    msg: 'Usuário criado com sucesso',
   });
 });
 
-app.put('/user', (req, res) => {
+app.put('/user/:id', (req, res) => {
+  users = users.map((user) => {
+    if (user.id === Number(req.params.id)) {
+      return {
+        ...user,
+        ...req.body,
+      };
+    }
+    return user;
+  });
+
   res.json({
-    msg: 'Atualizar um usuário na minha base',
+    msg: 'Usuário atualizado com sucesso',
   });
 });
 
-app.delete('/user', (req, res) => {
+app.delete('/user/:id', (req, res) => {
+  users = users.filter((user) => user.id !== Number(req.params.id));
+
   res.json({
-    msg: 'Remover um usuário na minha base',
+    msg: 'Usuário removido com sucesso',
   });
 });
 
